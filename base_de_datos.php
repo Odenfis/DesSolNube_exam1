e0138d860f2c74f6f4529194f6476a45b95ccfc8
yenif <yenifer.vargas@tecsup.edu.pe>
2023-09-26 14:14:01 -0500

    "conexion "

--- base_de_datos.php
+++ base_de_datos.php
@@ -1,15 +1,22 @@
 <?php
-$contraseña = ""; 
-$usuario = ""; 
-$nombreBaseDeDatos = ""; 
-
-$rutaServidor = ""; 
-$puerto = ""; 
+$database_url = getenv('postgresql://postgres:vC74w0XWPI3BZReiNHIH@containers-us-west-55.railway.app:6768/railway');
+if ($database_url === false) {
+    die("Las variables de entorno no están configuradas correctamente.");
+}
 
 try {
-    $base_de_datos = new PDO("pgsql:host=$rutaServidor;port=$puerto;dbname=$nombreBaseDeDatos", $usuario, $contraseña);
-    $base_de_datos->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (Exception){
-    echo"Ocurrio un error con la base de datos:".$e->getMessage();
+    $db_params = parse_url($database_url);
+    
+    $db_host = $db_params['containers-us-west-55.railway.app'];
+    $db_port = $db_params['6768'];
+    $db_name = ltrim($db_params['railway'], '/');
+    $db_user = $db_params['postgres'];
+    $db_password = $db_params['vC74w0XWPI3BZReiNHIH'];
+
+    $pdo = new PDO("pgsql:host=$db_host;port=$db_port;dbname=$db_name", $db_user, $db_password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexión exitosa a la base de datos.";
+} catch (PDOException $e) {
+    die("Error de conexión a la base de datos: " . $e->getMessage());
 }
 ?>
